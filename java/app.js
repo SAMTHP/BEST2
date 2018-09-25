a5218636f5a6fbd9273a895bf839dddf9acb9ba1
SAMTHP <43207100+SAMTHP@users.noreply.github.com>
2018-09-25 10:45:31 +0200

    Ajout du dossier java dédié au popup

---
+++ java/app.js
@@ -0,0 +1,12 @@
+var modal = document.getElementById("modal");
+
+function openModal() {
+	modal.style.top = "0px";
+
+}
+
+setTimeout(openModal,10000);
+
+function closeModal() {
+	modal.style.top = "-300px";
+}
